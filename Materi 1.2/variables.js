095a91b29014f0b057c4dc994ddb024f53141f85
Akbar Fikriawan <37275048+Strontium1@users.noreply.github.com>
2024-10-29 21:49:18 +0700

    Initial commit

---
+++ Materi 1.2/variables.js
@@ -0,0 +1,15 @@
+﻿var globalVar = "ini global var";
+let globalLet = "ini global let";
+const globalConst = "ini global const";
+
+console.log("globalVar: ", globalVar);
+console.log("globalLet: ", globalLet);
+console.log("globalConst: ", globalConst);
+
+globalVar = "global var diganti isinya";
+globalLet = "global let diganti isinya";
+//globalConst = "global const diganti isinya"; // menyebabkan error karena const tidak bisa diubah nilainya, dan jika diubah maka tidak bisa dirun
+
+console.log("globalVar (sekarang): ", globalVar);
+console.log("globalLet (sekarang): ", globalLet);
+console.log("globalConst (sekarang): ", globalConst);
